0b80eef4525938b020add800222d047a0994f65a
David McHugh <mcqman1@gmail.com>
2021-02-28 22:32:01 -0500

    Add build script for GCC.

---
+++ gccbuild.bat
@@ -0,0 +1,21 @@
+@echo off
+
+echo building for x86...
+    
+if not exist "bin\x86" (
+    mkdir "bin\x86"
+)
+
+windres "resources\icon.rc" -o "resources\icon.o" --target=pe-i386
+gcc "src\main.c" "resources\icon.o" -static -mwindows -m32 -lcomctl32 -o "bin\x86\Simple Memory Editor (32-bit).exe"
+del "resources\*.o" 2>NUL
+
+echo building for x64...
+    
+if not exist "bin\x64" (
+    mkdir "bin\x64"
+) 
+
+windres "resources\icon.rc" -o "resources\icon.o" --target=pe-x86-64
+gcc "src\main.c" "resources\icon.o" -static -mwindows -lcomctl32 -o "bin\x64\Simple Memory Editor (64-bit).exe"
+del "resources\*.o" 2>NUL
